6fbf4fa1b5ceb321600afa5397fe9e13dfafab87
okayblue <56899411+okayblue@users.noreply.github.com>
2022-05-30 20:50:46 -0400

    Add clear function, add functions to display numbers on screen

--- script.js
+++ script.js
@@ -1,3 +1,55 @@
+const calcButtons = document.querySelectorAll('button.calcItems');
+const numButton = document.querySelectorAll('.numButton')
+const equalsButton = document.querySelector('#equals');
+const clearButton = document.querySelector('#clear');
+const operationButtons = document.querySelectorAll('#add, #subtract, #multiply, #divide');
+const calcDisplay = document.querySelector('#display');
+const displayTop = document.querySelector('#top');
+const displayBottom = document.querySelector('#bottom');
+
+let num1 = '';
+let num2 = '';
+let op = '';
+
+numButton.forEach((button) => {
+    button.addEventListener('click', () => {
+        if (displayBottom.textContent == '0') {
+            displayBottom.textContent = '';
+        } 
+        if (!op) {
+            displayBottom.textContent += button.textContent;
+            num1 = num1 + button.textContent;
+            console.log(num1)
+        } else if (op) {
+            if(!num2) {
+                displayBottom.textContent = '';
+            }
+            displayBottom.textContent += button.textContent;
+            num2 = num2 + button.textContent;
+            console.log(num2)
+        }
+    });
+});
+
+operationButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        if (num1 && !op) {
+            op = button.textContent;
+            console.log(op);
+        }
+    });
+});
+
+clearButton.addEventListener('click', () => {
+    displayBottom.textContent = '';
+    displayTop.textContent = '';
+    num1 = '';
+    num2 = '';
+    op = '';
+    
+})
+
+
 function add(x, y) {
     return x + y;
 }
@@ -32,5 +84,3 @@ function operate(operator, num1, num2) {
     }
     return result;
 }
-
-

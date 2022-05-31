9811d45a62dde0ef16588f6d954d91e012bb007a
okayblue <56899411+okayblue@users.noreply.github.com>
2022-05-30 22:07:50 -0400

    Add error message if user divides by 0

--- script.js
+++ script.js
@@ -13,14 +13,14 @@ let op = '';
 
 numButton.forEach((button) => {
     button.addEventListener('click', () => {
-        if (displayBottom.textContent == '0') {
+        if (displayBottom.textContent == '0' || displayBottom.textContent == 'Error!') {
             displayBottom.textContent = '';
-        } 
+        }
         if (!op) {
             displayBottom.textContent += button.textContent;
             num1 = num1 + button.textContent;
         } else if (op) {
-            if(!num2) {
+            if (!num2) {
                 displayBottom.textContent = '';
             }
             displayBottom.textContent += button.textContent;
@@ -34,33 +34,52 @@ operationButtons.forEach((button) => {
         if (num1 && !op) {
             op = button.id;
         } else if (num2) {
-            let result = operate(op, Number(num1), Number(num2));
-            displayBottom.textContent = result;
-            num1 = result;
-            op = button.id;
-            num2 = '';
+            if (num2 == 0) {
+                divideByZero();
+            } else {
+                let result = operate(op, Number(num1), Number(num2));
+                displayBottom.textContent = result;
+                num1 = result;
+                op = button.id;
+                num2 = '';
+            }
+
         }
     });
 });
 
 equalsButton.addEventListener('click', () => {
-    if(num1 && num2 && op) {
-        let result = operate(op, Number(num1), Number(num2));
-        displayBottom.textContent = result;
-        num1 = result;
-        op = '';
-        num2 = '';
+    if (num1 && num2 && op) {
+        if (num2 == 0) {
+            divideByZero();
+        } else {
+            let result = operate(op, Number(num1), Number(num2));
+            displayBottom.textContent = result;
+            num1 = result;
+            op = '';
+            num2 = '';
+        }
+
     }
 })
 
 clearButton.addEventListener('click', () => {
+    clearCalculator();
+})
+
+function clearCalculator() {
     displayBottom.textContent = '';
     displayTop.textContent = '';
     num1 = '';
     num2 = '';
     op = '';
-})
+}
 
+function divideByZero() {
+    clearCalculator();
+    displayBottom.textContent = 'Error!';
+
+}
 
 function add(x, y) {
     return x + y;
